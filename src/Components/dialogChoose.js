9e8fcb1da438064543c269941f5fd4ad69fc38ad
declandeng <2633389934@qq.com>
2024-04-19 00:18:55 +0800

    feat: FIX chatPage error

--- src/Components/dialogChoose.js
+++ src/Components/dialogChoose.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import './dialogChoose.scss';
+import React from "react";
+import "./dialogChoose.scss";
 
 const ContactList = ({ contacts, selectedContact, onSelectContact }) => {
-    return (
-        <div className="contact-list">
-            {contacts.map((contact) => (
-                <div
-                    key={contact.id}
-                    className={`contact-item ${
-                        selectedContact === contact.id ? 'selected' : ''
-                    }`}
-                    onClick={() => onSelectContact(contact.id)}
-                >
-                    <img src={contact.avatar} alt={contact.name} />
-                    <div className="contact-name">{contact.name}</div>
-                </div>
-            ))}
+  return (
+    <div className="contact-list">
+      {contacts.map((contact) => (
+        <div
+          key={contact.id}
+          className={`contact-item ${
+            selectedContact === contact.id ? "selected" : ""
+          }`}
+          onClick={() => {}}
+        >
+          <img src={contact.avatar} alt={contact.name} />
+          <div className="contact-name">{contact.name}</div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ContactList;
--- src/Views/familyContact.js
+++ src/Views/familyContact.js
@@ -1,41 +1,51 @@
 import DialogWindow from "../Components/dialogWindow";
 import DialogChoose from "../Components/dialogChoose";
-import "./familyContact.scss"
-
+import "./familyContact.scss";
 
 const contacts = [
-    {
-        id: 1,
-        name: '张三',
-        avatar: 'https://randomuser.me/api/portraits/men/1.jpg',
-    },
-    {
-        id: 2,
-        name: '李四',
-        avatar: 'https://randomuser.me/api/portraits/women/2.jpg',
-    },
-    {
-        id: 3,
-        name: '王五',
-        avatar: 'https://randomuser.me/api/portraits/men/3.jpg',
-    },
-    {
-        id: 4,
-        name: '赵六',
-        avatar: 'https://randomuser.me/api/portraits/women/4.jpg',
-    }
+  {
+    id: 1,
+    name: "林天宇",
+    avatar: "https://en.pimg.jp/065/551/508/1/65551508.jpg",
+  },
+  {
+    id: 2,
+    name: "马云",
+    avatar:
+      "https://www.132jk.com/zb_users/upload/2023/10/202310131697171319680390.jpg",
+  },
+  {
+    id: 3,
+    name: "张伟",
+    avatar: "https://pic.616pic.com/ys_img/01/17/47/eVnQ1JdIY4.jpg",
+  },
+  {
+    id: 4,
+    name: "林天宇2",
+    avatar: "https://img.nanrentu.cc/listImg/c2023/11/09/eiqevgslyk0.jpg",
+  },
+  {
+    id: 5,
+    name: "林天宇3",
+    avatar: "https://static-cse.canva.cn/blob/353796/1600w-1tBhPU1AXV4.jpg",
+  },
+  {
+    id: 6,
+    name: "林天宇4",
+    avatar: "https://image.16pic.com/00/93/95/16pic_9395911_s.jpg",
+  },
 ];
 const contact = () => {
-    return (
-        <div className="contact">
-            <div className={"contact-left"}>
-                <DialogChoose contacts={ contacts }></DialogChoose>
-            </div>
-            <div className={"contact-main"}>
-                <DialogWindow></DialogWindow>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="contact">
+      <div className={"contact-left"}>
+        <DialogChoose contacts={contacts}></DialogChoose>
+      </div>
+      <div className={"contact-main"}>
+        <DialogWindow></DialogWindow>
+      </div>
+    </div>
+  );
+};
 
 export default contact;
